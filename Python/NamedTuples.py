579aaa8335ef985b9555b1e1b83705c707fe0d29
“RegChand” <regchand1@gmail.com>
2024-06-09 20:12:52 +1200

    move to python

--- Python/NamedTuples.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple
-Vision = namedtuple('Vision',['left', 'right'])
-print(issubclass(Vision,tuple)) #check if tuple is true
-vision = Vision(9.5, 8.8) #assign variables to tuple
-print(vision.left)
-print(vision.right)
-print(vision[0]) #same as left 
-print(vision[1]) #same as right
