8310f404f49d1cb1263d3b9507cda2682f28c8d2
“RegChand” <regchand1@gmail.com>
2025-06-21 21:41:35 +1200

    C Sharp Practice

--- CSharp/Hello World/obj/Debug/net9.0/Hello World.AssemblyInfo.cs
+++ CSharp/Hello World/obj/Debug/net9.0/Hello World.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa6300ec3fe80c8b8acf7dbe3b2e5fa9346ac6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3599b5aa07cde2c863372e38bca5771cdf6247c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
